9b52672dffc90d8ef889510c4d50593a593652f6
Capsonchik <Serega00790@mail.ru>
2024-09-06 14:27:52 +0300

    Хот фикс sideNav

--- src/components/sideBar/SideBar.tsx
+++ src/components/sideBar/SideBar.tsx
@@ -7,20 +7,25 @@ import GroupIcon from '@rsuite/icons/legacy/Group';
 import MagicIcon from '@rsuite/icons/legacy/Magic';
 import GearCircleIcon from '@rsuite/icons/legacy/GearCircle';
 import {useRouter} from "next/navigation";
+import {useState} from "react";
 
 export const SideBar = () => {
   const router = useRouter();
 
-  const handleNavigate = (route: string) => {
+  const [activeKey, setActiveKey] = useState('1')
+
+
+  const handleNavigate = (route: string, key: string) => {
     router.push(route)
+    setActiveKey(key)
   }
 
   return (
     <div className={styles.sideNavContainer}>
-      <Sidenav appearance={'subtle'} defaultOpenKeys={['3', '4']}>
+      <Sidenav appearance={'subtle'} defaultOpenKeys={['3', '4', '5']}>
         <Sidenav.Body className={styles.sideBody}>
-          <Nav activeKey="1">
-            <Nav.Item onClick={() => handleNavigate('/')} className={styles.sideItem} eventKey="1"
+          <Nav activeKey={activeKey}>
+            <Nav.Item onClick={() => handleNavigate('/', '1')} className={styles.sideItem} eventKey="1"
                       icon={<DashboardIcon/>}>
               Дашборд
             </Nav.Item>
@@ -28,20 +33,15 @@ export const SideBar = () => {
               Группы пользователей
             </Nav.Item>
             <Nav.Menu eventKey="4" title="Аналитика" icon={<MagicIcon/>}>
-              <Nav.Item onClick={() => handleNavigate('/long')} eventKey="4-1">Лонгитюдный круг</Nav.Item>
+              <Nav.Item onClick={() => handleNavigate('/long', '4-1')} eventKey="4-1">Лонгитюдный круг</Nav.Item>
               <Nav.Item eventKey="4-2">Пространство и время</Nav.Item>
-              <Nav.Item onClick={() => handleNavigate('/news')} eventKey="4-3">Инфо - поле</Nav.Item>
+              <Nav.Item onClick={() => handleNavigate('/news', '4-3')} eventKey="4-3">Инфо - поле</Nav.Item>
               <Nav.Item eventKey="4-4">Внешние факторы</Nav.Item>
               <Nav.Item eventKey="3-4">Результат</Nav.Item>
             </Nav.Menu>
-            <Nav.Menu eventKey="5" title="Settings" icon={<GearCircleIcon/>}>
-              <Nav.Item eventKey="5-1">Applications</Nav.Item>
-              <Nav.Item eventKey="5-2">Channels</Nav.Item>
-              <Nav.Item eventKey="5-3">Versions</Nav.Item>
-              <Nav.Menu eventKey="5-5" title="Custom Action">
-                <Nav.Item eventKey="5-5-1">Action Name</Nav.Item>
-                <Nav.Item eventKey="5-5-2">Action Params</Nav.Item>
-              </Nav.Menu>
+            <Nav.Menu eventKey="5" title="Настройки" icon={<GearCircleIcon/>}>
+              <Nav.Item eventKey="5-1">Профиль</Nav.Item>
+              <Nav.Item eventKey="5-2">Админ панель</Nav.Item>
             </Nav.Menu>
           </Nav>
         </Sidenav.Body>
