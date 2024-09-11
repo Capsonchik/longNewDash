18258a74f034dc97a05b706c89099f1268435e32
Capsonchik <Serega00790@mail.ru>
2024-09-11 17:33:15 +0300

    Хот фикс

--- src/components/slider/MainSlider.tsx
+++ src/components/slider/MainSlider.tsx
@@ -12,10 +12,10 @@ export const MainSlider = () => {
                      down={0.8} downStatus={'up'} change={'п.п'}/>
         <SliderBlock title={'Дефлятор FMCG'} percent={227.5} date={'август 2024'} up={2.3} upStatus={'up'} down={21.2}
                      downStatus={'up'} change={'%'}/>
-        <SliderBlock title={'Недельный средний чек'} percent={816} date={'2-8 сентября'} up={2.9} upStatus={'down'}
-                     down={11.2} downStatus={'up'} change={'%'}/>
-        <SliderBlock title={'Недельные расходы'} percent={60.5} date={'2-8 сентября'} up={2.4} upStatus={'up'}
-                     down={23.2} downStatus={'up'} change={'%'}/>
+        {/*<SliderBlock title={'Недельный средний чек'} percent={816} date={'2-8 сентября'} up={2.9} upStatus={'down'}*/}
+        {/*             down={11.2} downStatus={'up'} change={'%'}/>*/}
+        {/*<SliderBlock title={'Недельные расходы'} percent={60.5} date={'2-8 сентября'} up={2.4} upStatus={'up'}*/}
+        {/*             down={23.2} downStatus={'up'} change={'%'}/>*/}
       </div>
     </div>
   );
