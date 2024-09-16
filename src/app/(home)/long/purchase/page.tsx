d8271b9a18700c6a0a5e57e6d662d2ca9b1560d0
Capsonchik <Serega00790@mail.ru>
2024-09-16 11:59:15 +0300

    Новая страница с картой

--- src/app/(home)/long/purchase/page.tsx
+++ src/app/(home)/long/purchase/page.tsx
@@ -1,3 +1,35 @@
+'use client'
+
+import styles from './styles.module.scss'
+import {MapBig} from "@/components/mapBig/MapBig";
+import {REGION} from "@/mocks/regionInfoMock";
+import {SelectPicker} from "rsuite";
+import {useState} from "react";
+
 export default function Page() {
-  return <span>test</span>
+  const [currentRegion, setCurrentRegion] = useState<string | null>(null)
+
+  const data = REGION.map(item => ({label: item, value: item}));
+
+  const handlePickRegion = (value: string | null) => {
+    if (value) {
+      setCurrentRegion(value);
+      console.log(value);
+    } else setCurrentRegion(null);
+  }
+
+  return (
+    <div className={styles.container}>
+      <span className={styles.title}>Объем покупок по регионам</span>
+      <div className={styles.filters}>
+        <SelectPicker
+          data={data}
+          placeholder={'Выберите регион...'}
+          onChange={(value) => handlePickRegion(value)}
+        />
+        <SelectPicker data={data} placeholder={'Выберите категорию...'}/>
+      </div>
+      <MapBig currentValue={currentRegion}/>
+    </div>
+  )
 }
--- src/components/baseStatistic/widgets/mapWidget/MapWidget.tsx
+++ src/components/baseStatistic/widgets/mapWidget/MapWidget.tsx
@@ -1,10 +1,26 @@
+'use client'
+
 import styles from './styles.module.scss';
 import {Map} from "@/components/map/Map";
+import {useRouter} from "next/navigation";
+import {useDispatch} from "react-redux";
+import {AppDispatch} from "@/store/store";
+import {setActiveKey} from "@/store/store.slice";
+
 
 export const MapWidget = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const router = useRouter();
+
+  const handleNavigate = () => {
+    dispatch(setActiveKey('3'))
+    router.push('/long/purchase');
+  }
+
+
   return (
     <div className={styles.container}>
-      <span className={styles.title}>Объем покупок в категории</span>
+      <span onClick={handleNavigate} className={styles.title}>Объем покупок в категории</span>
       <Map/>
     </div>
   );
