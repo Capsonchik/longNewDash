31050031885a330eb67e2907a40cd6ca7318c2a2
Capsonchik <Serega00790@mail.ru>
2024-09-17 14:00:09 +0300

    Хот фикс

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,23 +1,23 @@
 import styles from './styles.module.scss';
-import { FiAlignJustify, FiInbox, FiBell, FiSettings} from "react-icons/fi";
+import {FiAlignJustify, FiBell, FiInbox, FiSettings} from "react-icons/fi";
 
 import * as React from 'react';
-import {Input} from "rsuite";
+import {Badge, Input} from "rsuite";
 
-type Props = {
-
-};
+type Props = {};
 export const Header = (props: Props) => {
   return (
     <div className={styles.header}>
       <div className={styles.leftBlock}>
         <span className={styles.logo}>РОМИР</span>
         <FiAlignJustify className={styles.menuIcon}/>
-        <Input className={styles.input} placeholder="Поиск по сервису..." />
+        <Input className={styles.input} placeholder="Поиск по сервису..."/>
       </div>
       <div className={styles.rightIcons}>
         <FiInbox className={styles.icon}/>
-        <FiBell className={styles.icon}/>
+        <Badge color={'blue'}>
+          <FiBell className={styles.icon}/>
+        </Badge>
         <FiSettings className={styles.icon}/>
       </div>
     </div>
