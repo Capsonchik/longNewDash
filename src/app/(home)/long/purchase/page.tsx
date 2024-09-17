7ba3478e03f88a7d9ab2558eb4c62f92269e9653
Capsonchik <Serega00790@mail.ru>
2024-09-17 16:23:58 +0300

    Добавление периодов в выборку

--- src/app/(home)/long/purchase/page.tsx
+++ src/app/(home)/long/purchase/page.tsx
@@ -3,10 +3,11 @@
 import styles from './styles.module.scss'
 import {MapBig} from "@/components/mapBig/MapBig";
 import {MAP_REGIONS} from "@/mocks/regionInfoMock";
-import {SelectPicker} from "rsuite";
+import {RangeSlider, SelectPicker} from "rsuite";
 import {useState} from "react";
 import {useSelector} from "react-redux";
 import {selectCurrentRegion} from "@/store/store.selectors";
+import {PERIOD} from "@/mocks/periodMock";
 
 export default function Page() {
   const region = useSelector(selectCurrentRegion)
@@ -14,6 +15,7 @@ export default function Page() {
   const [currentRegion, setCurrentRegion] = useState<string | null>(region)
 
   const data = MAP_REGIONS.map(item => ({label: item, value: item}));
+  const periodData = PERIOD.map(item => ({label: item, value: item}));
 
   const handlePickRegion = (value: string | null) => {
     if (value) {
@@ -33,6 +35,16 @@ export default function Page() {
           onChange={(value) => handlePickRegion(value)}
         />
         <SelectPicker data={data} placeholder={'Выберите категорию...'}/>
+        <SelectPicker defaultValue={'Год'} data={periodData} placeholder={'Период...'}/>
+      </div>
+      <div className={styles.slider}>
+        <RangeSlider
+          renderMark={(mark) => mark}
+          graduated
+          defaultValue={[2019, 2022]}
+          min={2018}
+          max={2024}
+        />
       </div>
       <MapBig currentValue={currentRegion}/>
     </div>
