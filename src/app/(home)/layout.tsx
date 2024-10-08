bfd09aefbcb04b529c85be2b497da20ec0838a5c
Capsonchik <Serega00790@mail.ru>
2024-10-08 12:32:25 +0300

    Хот фикс

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({children,}: Readonly<{ children: React.React
       <Header/>
       <div className={styles.content}>
         <SideBar/>
-        <RightContentContainer children={children}/>
+        <RightContentContainer>
+          {children}
+        </RightContentContainer>
         {/*<div className={styles.rightContent}>*/}
         {/*  <HeaderTitle/>*/}
         {/*  {children}*/}
--- src/components/rightContentContainer/RightContentContainer.tsx
+++ src/components/rightContentContainer/RightContentContainer.tsx
@@ -4,9 +4,10 @@ import styles from './styles.module.scss'
 import {useSelector} from "react-redux";
 import {selectIsMinMenu} from "@/store/store.selectors";
 import {HeaderTitle} from "@/components/header/headerTitle/HeaderTitle";
+import {ReactNode} from "react";
 
 
-export const RightContentContainer = ({children}: any) => {
+export const RightContentContainer = ({children,}: Readonly<{ children: ReactNode; }>) => {
   const isMinMenu = useSelector(selectIsMinMenu)
 
   return (
