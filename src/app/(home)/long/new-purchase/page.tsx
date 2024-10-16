d5cfff55f2640ada21286edd25d8444d64c6b50f
Capsonchik <Serega00790@mail.ru>
2024-10-16 15:20:02 +0300

    правки

--- src/app/(home)/long/new-purchase/page.tsx
+++ src/app/(home)/long/new-purchase/page.tsx
@@ -1,7 +1,8 @@
 import {MapWhite} from "@/components/mapWhite/MapWhite";
-import {Panel, SelectPicker} from "rsuite";
+import {Panel, SelectPicker, Text} from "rsuite";
 import {MAP_REGIONS} from "@/mocks/regionInfoMock";
 import {MockScatter} from "@/components/charts/mockChart/mockScatter";
+import {MonthPiker} from "@/components/rangePickers/monthPicker/MonthPiker";
 
 export default function Page() {
   const data = MAP_REGIONS.map(item => ({label: item, value: item}));
@@ -10,13 +11,38 @@ export default function Page() {
     <div style={{display: "flex", justifyContent: "space-between"}}>
       <Panel bordered style={{width: '49%'}}>
         <div style={{display: "flex", flexDirection: "column", rowGap: '1rem'}}>
-          <SelectPicker data={data} defaultValue={'Москва'} placeholder={'Выберите регион'}/>
+          <div style={{display: "flex", columnGap: '1rem'}}>
+            <SelectPicker data={data} placeholder={'Выберите показатель'}/>
+            <MonthPiker/>
+          </div>
           <MapWhite currentValue={''}/>
         </div>
       </Panel>
       <Panel bordered style={{width: '49%'}}>
         <div style={{width: '100%'}}>
-          <MockScatter/>
+          <div style={{display: "flex", columnGap: '1rem', marginBottom: '1rem'}}>
+            <SelectPicker data={data} placeholder={'Параметр 1'}/>
+            <SelectPicker data={data} placeholder={'Параметр 2'}/>
+          </div>
+          <div style={{display: 'flex', width: '100%'}}>
+            <div style={{width: '70%'}}>
+              <MockScatter/>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'column', gap: '1rem'}}>
+              <Text>Коэффициент корреляции</Text>
+              <div style={{
+                width: 100,
+                height: 100,
+                backgroundColor: '#80c783',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                borderRadius: 16
+              }}>
+                <Text weight={'bold'} size={'xxl'}>+7.9</Text>
+              </div>
+            </div>
+          </div>
         </div>
       </Panel>
     </div>
